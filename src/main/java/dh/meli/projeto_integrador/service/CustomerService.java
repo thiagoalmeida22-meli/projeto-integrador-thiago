b3bf29c8b6a2301dbb191139bcc8bc281c555eef
thiagoalmeida22-meli <thiago.guimaraes@mercadolivre.com>
2022-08-17 01:08:14 -0300

    FEAT: Criação de um customerService

--- src/main/java/dh/meli/projeto_integrador/service/CustomerService.java
+++ src/main/java/dh/meli/projeto_integrador/service/CustomerService.java
@@ -1,2 +1,36 @@
-package dh.meli.projeto_integrador.service;public class CustomerService {
+package dh.meli.projeto_integrador.service;
+
+import dh.meli.projeto_integrador.exception.ResourceNotFoundException;
+import dh.meli.projeto_integrador.model.Customer;
+import dh.meli.projeto_integrador.model.Warehouse;
+import dh.meli.projeto_integrador.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService implements ICustomerService{
+
+	/**
+	 * Dependency Injection of the Warehouse Repository.
+	 */
+	@Autowired
+	private ICustomerRepository customerRepository;
+
+	/**
+	 * Method to find a warehouse by id;
+	 * @param id of type long. Warehouse identifier;
+	 * @return an object of type Warehouse;
+	 */
+	@Override
+	public Customer findCustomer(long id) {
+		Optional<Customer> customer = customerRepository.findById(id);
+
+		if (customer.isEmpty()) {
+			throw new ResourceNotFoundException(String.format("Could not find valid customer for id %d", id));
+		}
+
+		return customer.get();
+	}
 }
